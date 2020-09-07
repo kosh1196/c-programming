df69a0774acf967462127d4af208a4fc1fa12cfe
Coursera Learner <nobody@nowhere.nul>
2020-09-07 04:59:46 +0000

    retirement

--- 07_retirement/retirement.c
+++ 07_retirement/retirement.c
@@ -1,77 +1,57 @@
+#include <stdlib.h>
 #include <stdio.h>
 
-#include <stdlib.h>
-
 struct _retire_info {
-
   int months;
-
   double contribution;
-
   double rate_of_return;
-
 };
 
 typedef struct _retire_info retire_info;
 
-int calculator (int startAge, double initial, retire_info status) {
+double compute_savings(double balance, retire_info data) {
+  return data.contribution + balance * data.rate_of_return;
+}
 
-  int month = status.months;
-
-  int currentAge = startAge;
-
-  double balance = initial;
-
-  for (int i = 1; i <= month; i++) {
-
-    int years = currentAge / 12;
-
-    int mon = currentAge % 12;
-
-    double interest = balance * status.rate_of_return;
-
-    balance = balance + interest + status.contribution;
-
-    printf("Age %3d month %2d you have %.2f\n", years, mon, balance);
-
-    currentAge++;
-
-  }
-
-  return balance;
-
+void print_balance(int age, double balance) {
+  printf("Age %3d month %2d you have $%.2lf\n", age / 12, age % 12, balance);
 }
 
 void retirement (int startAge, double initial, retire_info working, retire_info retired) {
 
-  printf("Age %3d month %2d you have %.2f\n", startAge / 12, startAge % 12, initial);
+  double balance = initial;
+  int age = startAge;
 
-  double finalBalance = calculator(328, 21345, working);
+  // working time
+  for (int i = 0; i < working.months; i++) {
+    print_balance(age, balance);
 
-  calculator(817, finalBalance, retired);
+    // compute new account balance every month
+    balance += compute_savings(balance, working);
+    age++;
+  }
 
+  // retirement
+  for (int i = 0; i < retired.months; i++) {
+    print_balance(age, balance);
+
+    balance += compute_savings(balance, retired);
+    age++;
+  }
 }
 
 int main (void) {
-
   retire_info working;
-
   working.months = 489;
-
   working.contribution = 1000;
+  working.rate_of_return = 0.045 / 12;
 
-  working.rate_of_return = 0.045/12;
+  retire_info retired = { .months = 384, .contribution = -4000.0, .rate_of_return = 0.01 / 12};
 
-  retire_info retired;
+  int age = 327;
+  double savings = 21345.0;
 
-  retired.months = 383;
+  retirement(age, savings, working, retired);
 
-  retired.contribution = (-4000);
-
-  retired.rate_of_return = 0.01/(double)12;
-
-  retirement (327, 21345, working, retired);
-
-  return 0;
-
+  return EXIT_SUCCESS;
 }

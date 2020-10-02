d6d37f05aaf2f05f83e9ba47af7a5a37c35861d8
Coursera Learner <nobody@nowhere.nul>
2020-10-02 06:55:01 +0000

    ddi 20 rotate

---
+++ 20_rot_matrix/rotate.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void rotate(char matrix[10][10])
+{
+  char rot[10][10];
+  for(int i=0; i<10;i++){
+    for(int j=0; j<10;j++){
+      rot[i][j] = matrix[i][j];
+    }
+  } 
+  for(int i=0; i<10; i++){
+    for(int j=0; j<10; j++){
+      matrix[i][j] = rot[9-j][i];
+    }
+  }
+}

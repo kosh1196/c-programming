f9f9883d8770d6fee4bd9ad98bf67ba4486de121
Coursera Learner <nobody@nowhere.nul>
2020-10-08 06:47:45 +0000

    did 25 break encr

--- 25_break_encr/breaker.c
+++ 25_break_encr/breaker.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
+#include <ctype.h>
 
-void getFreqArray( int *freqArr, FILE *f)
-{
+void getFreqArray(int *freqArr, FILE * f){
   int c;
   while((c=fgetc(f)) != EOF){
     if(isalpha(c)){
       c = tolower(c);
       c = c - 'a';
-      freqArr[c] = freqArr[c] + 1;
+      freqArr[c] = freqArr[c]+1;
     }
   }
 }
 
-int maxFreqIdx(int *freqArr)
-{
+int maxFreqIdx(int * freqArr){
   int max = freqArr[0];
   int maxIdx = 0;
   for(int i=0; i<26; i++){
-    if(freqArr[i] > max){
+    if(freqArr[i]>max){
       max = freqArr[i];
       maxIdx = i;
     }
@@ -28,20 +26,19 @@ int maxFreqIdx(int *freqArr)
   return maxIdx;
 }
 
-int main(int argc, char **argv)
-{
-  if(argc != 2){
-    fprintf(stderr,"Usage: Wrong arguments\n");
+int main(int argc, char ** argv){
+  if(argc!=2){
+    fprintf(stderr, "Usage: Wrong amount of Arguments\n");
     return EXIT_FAILURE;
   }
-  FILE *f = fopen(argv[1], "r");
+  FILE * f = fopen(argv[1],"r");
   if(f==NULL){
-    perror("Could not open file.");
+    perror("Could not open file");
     return EXIT_FAILURE;
   }
   int freqArr[26] = {0};
-  getFreqArray(freqArr , f);
+  getFreqArray(freqArr, f);
   int maxIdx = maxFreqIdx(freqArr);
   int key = (maxIdx + 'a' - 'e' + 26)%26;
-  printf("%d \n ", key);
+  printf("%d\n", key);
 }

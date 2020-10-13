fd1a4d83194ebd76b560d52f75303dd34ae701be
Coursera Learner <nobody@nowhere.nul>
2020-10-13 06:34:08 +0000

    did 29 outname

--- 29_outname/outname.c
+++ 29_outname/outname.c
@@ -5,4 +5,9 @@
 
 char * computeOutputFileName(const char * inputName) {
   //WRITE ME
+  char *outName = malloc((strlen(inputName)+4) *sizeof(*outName));
+  strcpy(outName, inputName);
+  outName = realloc(outName,(strlen(inputName)+14) *sizeof(*outName));
+  strcat(outName, ".counts");
+  return outName;
 }

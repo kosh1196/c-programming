8b0fcffa4b3e498b8c18bed763f6376a4d77b5e9
Coursera Learner <nobody@nowhere.nul>
2020-10-14 06:55:42 +0000

    did 33 counts

--- 33_counts/counts.c
+++ 33_counts/counts.c
@@ -4,14 +4,53 @@
 #include "counts.h"
 counts_t * createCounts(void) {
   //WRITE ME
+  counts_t * counts = malloc(sizeof(*counts));
+  counts->arr = NULL;
+  counts->size = 0;
+  counts->unknown = 0;
+  return counts;
 }
 void addCount(counts_t * c, const char * name) {
   //WRITE ME
+  if(!name) c->unknown++;
+  else{
+    one_count_t * x = NULL;
+    int checker = 0;
+    for(size_t i=0; i<c->size; i++){
+      x = c->arr[i];
+      if(!strcmp(x->str, name)){
+	x->count++;
+	checker=1;
+      }
+    }
+    if(!checker){
+      x = NULL;
+      x = malloc(sizeof(*x));
+      x->str = malloc(strlen(name)+4);
+      strcpy(x->str, name);
+      x->count=1;
+      c->arr = realloc(c->arr, (c->size+1)*sizeof(*c->arr));
+      c->arr[c->size]=x;
+      c->size++;
+    }
+  }
 }
 void printCounts(counts_t * c, FILE * outFile) {
   //WRITE ME
+  one_count_t * x = NULL;
+  for(size_t i=0; i<c->size; i++){
+    x=c->arr[i];
+    fprintf(outFile, "%s: %zd\n", x->str, x->count);
+  }
+  if(c->unknown!=0) fprintf(outFile, "<unknown> : %zd\n", c->unknown);
 }
 
 void freeCounts(counts_t * c) {
   //WRITE ME
+  for(size_t i=0; i<c->size; i++){
+    free(c->arr[i]->str);
+    free(c->arr[i]);
+  }
+  free(c->arr);
+  free(c);
 }
--- 33_counts/counts.h
+++ 33_counts/counts.h
@@ -2,12 +2,16 @@
 #define __COUNTS_H__
 struct _one_count_t {
   //DEFINE ME
-
+  char * str;
+  size_t count;
 };
 typedef struct _one_count_t one_count_t;
 
 struct _counts_t {
   //DEFINE ME
+  one_count_t ** arr;
+  size_t size;
+  size_t unknown;
 };
 typedef struct _counts_t counts_t;
 
